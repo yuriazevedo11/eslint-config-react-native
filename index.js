b6ded9ae66f8f2eb51c92f9a2000cf81c602b9cc
Yuri Azevedo <yuriazevedo11@gmail.com>
2022-04-07 12:18:57 -0300

    ♻️ refactor: update Eslint rules

--- index.js
+++ index.js
@@ -1,64 +1,40 @@
 module.exports = {
   env: {
     jest: true,
-    es2020: true,
   },
-  extends: ['airbnb', 'prettier', 'prettier/react'],
+  extends: [
+    '@react-native-community',
+    'plugin:import/errors',
+    'plugin:import/typescript',
+  ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
     __DEV__: true,
   },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 11,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-    'jsx-a11y',
-    'import',
-    'react-hooks',
-    'prettier',
-    '@typescript-eslint',
-    'eslint-plugin-import-helpers',
-  ],
   rules: {
-    camelcase: 'off',
-    'global-require': 'off',
-    'no-param-reassign': 'off',
-    'no-underscore-dangle': 'off',
-    'no-console': ['error', { allow: ['tron'] }],
-    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    'react-native/no-raw-text': 'off',
-    'react/prop-types': 'off',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    'react/jsx-one-expression-per-line': 'off',
-    'react/jsx-filename-extension': [
-      'error',
-      { extensions: ['.js', '.jsx', '.tsx'] },
-    ],
-    'jsx-a11y/anchor-is-valid': 'off',
-    'import/prefer-default-export': 'off',
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
-    'import-helpers/order-imports': [
+    'no-console': 'error',
+    'react-native/no-inline-styles': 'error',
+    'import/no-duplicates': 'error',
+    'import/newline-after-import': 'error',
+    'import/order': [
       'error',
       {
-        newlinesBetween: 'always',
-        groups: ['module', '/^@//', ['parent', 'sibling', 'index']],
+        'newlines-between': 'always',
+        groups: [
+          'builtin',
+          'external',
+          ['internal', 'parent'],
+          ['sibling', 'index'],
+          'type',
+          'object',
+        ],
+        pathGroups: [
+          {
+            pattern: '~/**',
+            group: 'internal',
+          },
+        ],
       },
     ],
   },
@@ -66,16 +42,20 @@ module.exports = {
     {
       files: ['*.ts', '*.tsx'],
       rules: {
-        '@typescript-eslint/no-unused-vars': [2, { args: 'none' }],
+        'no-undef': 'off',
+        'no-shadow': 'off',
+        '@typescript-eslint/no-shadow': ['warn'],
       },
     },
   ],
   settings: {
+    'import/ignore': ['node_modules/react-native/index\\.js$'],
     'import/resolver': {
       typescript: {},
       node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        extensions: ['.ts', '.tsx', '.d.ts', '.js', '.jsx'],
       },
     },
   },
+  ignorePatterns: ['node_modules/', 'coverage/', 'android/', 'ios/'],
 };
